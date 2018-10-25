226199a3fa5ba2917d96bbd0739daedf8f7c54cb
Cameron Beeler <cameron.beeler@gmail.com>
2018-10-25 15:11:32 -0700

    Finished with simple example of thread prioritization and starvation

--- src/com/cameronbeeler/Main.java
+++ src/com/cameronbeeler/Main.java
@@ -2,7 +2,53 @@ package com.cameronbeeler;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static Object lock = new Object();
+
+    public static void main(String[] args)
+    {
+        Thread t1 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 10");
+        Thread t2 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 8");
+        Thread t3 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 6");
+        Thread t4 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "Priority 4");
+        Thread t5 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 2");
+/*
+        t1.setPriority(10);
+        t1.setPriority(8);
+        t1.setPriority(6);
+        t1.setPriority(4);
+        t1.setPriority(2);
+        */
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+    }
+
+    private static
+    class Worker
+    implements Runnable
+    {
+
+        private int runCount = 0;
+        private String threadColor;
+
+        public Worker(String threadColor)
+        {
+            this.threadColor = threadColor;
+        }
+        @Override
+        public
+        void run()
+        {
+            for(int i=0;i<100;i++)
+            {
+                synchronized (lock)
+                {
+                    System.out.format(threadColor + "%s:  runCount = %d\n,", Thread.currentThread().getName(), runCount++);
+                }
+            }
+        }
     }
 }
